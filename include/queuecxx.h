297a9d06a883cd853ed3daaddb1451d3b83a8d21
Abdullah Selek <abdullahselek@gmail.com>
2017-05-29 14:41:17 +0200

    create lib structure

---
+++ include/queuecxx.h
@@ -0,0 +1,8 @@
+//
+// Created by Selek, Abdullah on 29.05.17.
+//
+
+#ifndef QUEUECXX_QUEUECXX_H
+#define QUEUECXX_QUEUECXX_H
+
+#endif //QUEUECXX_QUEUECXX_H
---
+++ source/queuecxx.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Selek, Abdullah on 29.05.17.
+//
+
